21a17d64b67c78db37df0c6e600a0c0ddc94ba36
user <bexruznegmurodov2808@gmail.com>
2023-07-10 21:16:41 +0500

    templates

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AccauntConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accaunt'
+    name = 'accounts'
